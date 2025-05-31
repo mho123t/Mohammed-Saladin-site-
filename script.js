a6380a29536c5b5e01f3084009cc9c07862b7560
mho123t <noorcombsh@gmail.com>
2025-05-31 18:58:25 +0400

    Update script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-javascript
-function toggleLang() {
-  const lang = document.documentElement.getAttribute('lang');
-  if (lang === 'ar') {
-    window.location.href = window.location.pathname.replace('.html', '_en.html') || 'index_en.html';
-  } else {
-    window.location.href = window.location.pathname.replace('_en.html', '.html') || 'index.html';
-  }
-}
